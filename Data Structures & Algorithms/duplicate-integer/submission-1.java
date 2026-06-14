666913300d8fea13b654ee2d7209beca24d3c112
amitabh-29 <amitabh29chaubey@gmail.com>
2026-06-14 13:04:11 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+if(nums[i]==nums[i+1]){
+    return true;
+}
+i++;
+
+        }
+        return false;
+    }
+}
