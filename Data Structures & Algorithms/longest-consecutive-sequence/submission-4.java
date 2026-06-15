1cf8995ee471659757a6bb430a836ac20941bd79
amitabh-29 <amitabh29chaubey@gmail.com>
2026-06-15 10:32:32 +0530

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+    Arrays.sort(nums);
+    int Maxcount=1;
+    int count=1;
+    for(int i=1;i<nums.length;i++){
+        // skip duplicates
+         if(nums[i]==nums[i-1]){
+            continue;
+         }
+         if(nums[i]==nums[i-1]+1){
+            count++;
+         }
+         else {
+            Maxcount=Math.max(Maxcount,count);
+            count=1;
+         }
+    }
+return Math.max(Maxcount,count);
+    }
+}
